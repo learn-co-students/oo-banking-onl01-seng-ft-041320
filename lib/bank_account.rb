677e593b091658b3617c0d001b781626e331646f
osha7 <ogroetz@gmail.com>
2020-04-29 16:05:58 -0600

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,74 @@
 class BankAccount
 
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    def initialize (name, balance = 1000, status = "open")
+        @name = name
+        @balance = 1000
+        @status = status
+        
+    end
+
+    def deposit(deposit)
+        self.balance += deposit
+    end
+
+    def display_balance
+        "Your balance is $#{@balance}."
+    end
+
+    def valid?
+        if @status == "open" && @balance > 0
+            true
+        else
+            false
+        end
+    end
+
+    def close_account
+       @status = "closed"
+    end
+
+
+
 end
+
+
+    
+      
+    #     describe '#deposit' do
+    #       it "can deposit money into its account" do
+    #         expect(avi.balance).to eq(1000)
+    #         avi.deposit(1000)
+    #         expect(avi.balance).to eq(2000)
+    #       end
+    #     end
+      
+    #     describe '#display_balance' do
+    #       it "can display its balance" do
+    #         expect(avi.display_balance).to eq("Your balance is $#{avi.balance}.")
+    #       end
+    #     end
+      
+    #     describe '#valid?' do
+    #       it "is valid with an open status and a balance greater than 0" do
+    #         @broke = BankAccount.new("Kat Dennings")
+    #         @broke.balance = 0
+    #         @closed = BankAccount.new("Beth Behrs")
+    #         @closed.status = "closed"
+    #         expect(avi.valid?).to eq(true)
+    #         expect(@broke.valid?).to eq(false)
+    #         expect(@closed.valid?).to eq(false)
+    #       end
+    #     end
+      
+    #     describe '#close_account' do
+    #       it "can close its account" do
+    #         avi.close_account
+    #         expect(avi.status).to eq("closed")
+    #       end
+    #     end
+    #   end
+      
+
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,95 @@
+require 'pry'
+
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize (sender, receiver, status = "pending", transfer_amount)
+    @sender = sender
+    @receiver = receiver 
+    @status = status
+    @amount = transfer_amount
+  end
+
+ 
+
+  def valid?
+    if sender.valid? && receiver.valid?
+      true
+    else
+      false
+    end
+
+  end
+
+  def execute_transaction
+
+    if self.valid? && sender.balance >= self.amount && self.status != "complete"
+      sender.balance -= self.amount
+      receiver.balance += self.amount
+      self.status = "complete"
+    else
+      self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+    end
+
+  end
+
+  def reverse_transfer
+     if @status == "complete"
+       @receiver.balance -= self.amount
+       @sender.balance += self.amount
+       self.status = "reversed"
+     end
+
+  end
+
+
 end
+
+#     it "can execute a successful transaction between two accounts" do
+#       transfer.execute_transaction
+#       expect(amanda.balance).to eq(950)
+#       expect(avi.balance).to eq(1050)
+#       expect(transfer.status).to eq("complete")
+#     end
+
+#     it "each transfer can only happen once" do
+#       transfer.execute_transaction
+#       expect(amanda.balance).to eq(950)
+#       expect(avi.balance).to eq(1050)
+#       expect(transfer.status).to eq("complete")
+#       transfer.execute_transaction
+#       expect(amanda.balance).to eq(950)
+#       expect(avi.balance).to eq(1050)
+#     end
+
+#     it "rejects a transfer if the sender does not have enough funds (does not have a valid account)" do
+#       terrance.close_account
+#       closed_account_transfer = Transfer.new(amanda, terrance, 50)
+#       expect(closed_account_transfer.execute_transaction).to eq("Transaction rejected. Please check your account balance.")
+#       expect(closed_account_transfer.status).to eq("rejected")
+
+#       expect(bad_transfer.execute_transaction).to eq("Transaction rejected. Please check your account balance.")
+#       expect(bad_transfer.status).to eq("rejected")
+#     end
+#   end
+
+#   describe '#reverse_transfer' do
+#     it "can reverse a transfer between two accounts" do
+#       transfer.execute_transaction
+#       expect(amanda.balance).to eq(950)
+#       expect(avi.balance).to eq(1050)
+#       transfer.reverse_transfer
+#       expect(avi.balance).to eq(1000)
+#       expect(amanda.balance).to eq(1000)
+#       expect(transfer.status).to eq("reversed")
+#     end
+
+#     it "it can only reverse executed transfers" do
+#       transfer.reverse_transfer
+#       expect(amanda.balance).to eq(1000)
+#       expect(avi.balance).to eq(1000)
+#     end
+#   end
+# end
